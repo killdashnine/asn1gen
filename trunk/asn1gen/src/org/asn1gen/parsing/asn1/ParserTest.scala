ce7524ebfd52c1439c2b4ec8bf28873817636920
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-10-17 02:37:35 +0000

    Test cases.

--- trunk/asn1gen/src/org/asn1gen/parsing/asn1/ParserTest.scala
+++
@@ -1,8 +0,0 @@
-package org.asn1gen.parsing.asn1
-
-object ParserTest {
-  def main(args : Array[String]) : Unit = {
-    def result = Asn1.parse("/*_modulereference_*/");
-    println(result);
-  }
-}
