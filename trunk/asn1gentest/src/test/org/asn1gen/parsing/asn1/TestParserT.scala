aea6e28ce4da85054898cf0593f746c53643799c
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-10-26 13:36:30 +0000

    Move files from asn1gen project.

---
+++ trunk/asn1gentest/src/test/org/asn1gen/parsing/asn1/TestParserT.scala
@@ -0,0 +1,96 @@
+import _root_.org.junit._
+import _root_.org.junit.Assert._
+import _root_.junit.framework.TestCase
+import _root_.org.asn1gen.parsing.asn1.Parser
+import _root_.org.asn1gen.parsing.asn1.ast._
+import _root_.scala.util.parsing.input._
+
+package test.org.asn1gen.parsing.asn1 {
+  class TestParserT extends TestCase {
+    
+    object TheParser extends Parser {
+      def parse[N](root : Parser[N], input: String) =
+        phrase(root)(new lexical.Scanner(input))
+    }
+    
+    import TheParser._
+    
+    @Test def test_type_1() {
+      parse(type_, "INTEGER { a(1), b(2), c(3) }") match {
+        case Success(
+          result@Type(_),
+          _) =>
+        case x => fail("Parse 'type_' failure: " + x)
+      }
+    }
+    
+    @Test def test_type_2() {
+      parse(type_, "CHOICE { choice1 [0] INTEGER, choice2 [1] INTEGER }") match {
+        case Success(
+          result@Type(
+            BuiltinType(
+              ChoiceType(
+                AlternativeTypeLists(
+                  RootAlternativeTypeList(
+                    List(
+                      NamedType(
+                        Identifier("choice1"),
+                        Type(
+                          BuiltinType(
+                            DefaultTaggedType(
+                              Tag(DefaultClass(), LiteralClassNumber(Number("0"))),
+                              Type(
+                                BuiltinType(IntegerType(List()))))))),
+                      NamedType(
+                        Identifier("choice2"),
+                        Type(
+                          BuiltinType(
+                            DefaultTaggedType(
+                              Tag(DefaultClass(), LiteralClassNumber(Number("1"))),
+                              Type(BuiltinType(IntegerType(List()))))))))),
+                  None,None,None))))
+,
+          _) => println(result)
+        case x => fail("Parse 'type_' failure: " + x)
+      }
+    }
+    
+    @Test def test_typeAssignment_1() {
+      parse(typeAssignment, "MyChoice ::= CHOICE { choice1 [0] INTEGER, choice2 [1] INTEGER }") match {
+        case Success(
+          result,
+          _) =>
+        case x => fail("Parse 'type' failure: " + x)
+      }
+    }
+    
+    // 9.1.1
+    @Test def test_typeAssignment_2() {
+      parse(typeAssignment, "TypeReference ::= CHOICE { integer INTEGER, boolean BOOLEAN }") match {
+        case Success(
+          TypeAssignment(
+            TypeReference("TypeReference"),
+            Type(BuiltinType(ChoiceType(AlternativeTypeLists(
+              RootAlternativeTypeList(
+                List(
+                  NamedType(Identifier("integer"),Type(BuiltinType(IntegerType(List())))),
+                  NamedType(Identifier("boolean"),Type(BuiltinType(BooleanType()))))),
+              None,
+              None,
+              None))))),
+          _) =>
+        case x => fail("Parse 'type' failure: " + x)
+      }
+    }
+    
+    // 9.1.1
+    @Test def test_typeAssignment_3() {
+      parse(typeAssignment, "Pair ::= SEQUENCE { x INTEGER, y INTEGER }") match {
+        case Success(
+          result,
+          _) => println(result)
+        case x => fail("Parse 'type' failure: " + x)
+      }
+    }
+  }
+}
