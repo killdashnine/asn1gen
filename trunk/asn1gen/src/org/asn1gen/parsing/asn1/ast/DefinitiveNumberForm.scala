89d3f66c055a47527daa4ebb6687b990a92ea305
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-10-29 10:12:38 +0000

    Generate more AST nodes.

--- trunk/asn1gen/src/org/asn1gen/parsing/asn1/ast/DefinitiveNumberForm.scala
+++ trunk/asn1gen/src/org/asn1gen/parsing/asn1/ast/DefinitiveNumberForm.scala
@@ -1,7 +1,8 @@
 package org.asn1gen.parsing.asn1.ast
 
+import org.asn1gen.parsing.asn1.ast.kind._
+
 case class DefinitiveNumberForm(
   number: Number
-) extends Node {
+) extends Node with DefinitiveObjectIdComponentKind {
 }
-
