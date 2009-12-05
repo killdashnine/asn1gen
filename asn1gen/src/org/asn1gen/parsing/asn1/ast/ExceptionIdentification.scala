f2f081849aebbd6a68a19a9ef4a429ba445b6a6e
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-05 13:46:36 +0000

    Generate children AST nodes for ExceptionIdentification.

--- asn1gen/src/org/asn1gen/parsing/asn1/ast/ExceptionIdentification.scala
+++ asn1gen/src/org/asn1gen/parsing/asn1/ast/ExceptionIdentification.scala
@@ -2,5 +2,5 @@ package org.asn1gen.parsing.asn1.ast
 
 import org.asn1gen.parsing.asn1.ast.kind._
 
-case class ExceptionIdentification() extends Node with ExceptionSpecKind {
+trait ExceptionIdentification extends ExceptionSpecKind {
 }
