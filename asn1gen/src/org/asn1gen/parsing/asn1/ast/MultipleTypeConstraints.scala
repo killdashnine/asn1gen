757e8dd73274b110736e8f01c812d740a5756084
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-05 13:50:48 +0000

    Generate children AST nodes for InnerTypeConstraints.

--- asn1gen/src/org/asn1gen/parsing/asn1/ast/MultipleTypeConstraints.scala
+++ asn1gen/src/org/asn1gen/parsing/asn1/ast/MultipleTypeConstraints.scala
@@ -4,5 +4,5 @@ import org.asn1gen.parsing.asn1.ast.kind._
 
 case class MultipleTypeConstraints(
   kind: MultipleTypeConstraintsKind
-) extends Node {
+) extends Node with InnerTypeConstraints {
 }
