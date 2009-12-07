2869dfa0a00ffc9155a374deab6d0382db4920aa
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-07 11:29:24 +0000

    Convert DefinedObject class into a trait.

--- trunk/asn1gen/src/org/asn1gen/parsing/asn1/ast/DefinedObject.scala
+++ trunk/asn1gen/src/org/asn1gen/parsing/asn1/ast/DefinedObject.scala
@@ -2,9 +2,7 @@ package org.asn1gen.parsing.asn1.ast
 
 import org.asn1gen.parsing.asn1.ast.kind._
 
-case class DefinedObject(
-  kind: DefinedObjectKind
-) extends Node
-  with ObjectKind
+trait DefinedObject
+  extends ObjectKind
   with ReferencedObjectsKind {
 }
