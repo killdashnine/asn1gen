c887373435917c5436dc3d23f8b0d59a68660d94
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-07 12:50:15 +0000

    Convert CharacterStringValue class into a trait.

--- trunk/asn1gen/src/org/asn1gen/parsing/asn1/ast/RestrictedCharacterStringValue.scala
+++ trunk/asn1gen/src/org/asn1gen/parsing/asn1/ast/RestrictedCharacterStringValue.scala
@@ -1,6 +1,4 @@
 package org.asn1gen.parsing.asn1.ast
 
-import org.asn1gen.parsing.asn1.ast.kind._
-
-trait RestrictedCharacterStringValue extends Node with CharacterStringValueKind {
+trait RestrictedCharacterStringValue extends Node with CharacterStringValue {
 }
