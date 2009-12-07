6a768fdc08c84879ee30621d2f4d30c042203896
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-07 11:45:23 +0000

    Convert NumericRealValue class into a trait.

--- asn1gen/src/org/asn1gen/parsing/asn1/ast/SequenceValue.scala
+++ asn1gen/src/org/asn1gen/parsing/asn1/ast/SequenceValue.scala
@@ -4,5 +4,5 @@ import org.asn1gen.parsing.asn1.ast.kind._
 
 case class SequenceValue(
   namedValues: List[NamedValue]
-) extends Node with BuiltinValue with NumericRealValueKind {
+) extends Node with BuiltinValue with NumericRealValue {
 }
