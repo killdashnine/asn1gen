be22597ac912f6dd5004aff376727b78f8235803
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-07 11:55:41 +0000

    Convert ObjIdComponents class into a trait.

--- asn1gen/src/org/asn1gen/parsing/asn1/ast/NameForm.scala
+++ asn1gen/src/org/asn1gen/parsing/asn1/ast/NameForm.scala
@@ -1,10 +1,8 @@
 package org.asn1gen.parsing.asn1.ast
 
-import org.asn1gen.parsing.asn1.ast.kind._
-
 case class NameForm(
   identifier: Identifier
 ) extends Node
   with DefinitiveObjectIdComponent
-  with ObjIdComponentsKind {
+  with ObjIdComponents {
 }
