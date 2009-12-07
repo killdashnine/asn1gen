c1daa78dcdf08d16ddb6f0fbf5e708f234028cfb
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-07 11:48:00 +0000

    Convert BitStringValue class into a trait.

--- asn1gen/src/org/asn1gen/parsing/asn1/ast/IdentifierList.scala
+++ asn1gen/src/org/asn1gen/parsing/asn1/ast/IdentifierList.scala
@@ -1,9 +1,6 @@
 package org.asn1gen.parsing.asn1.ast
 
-import org.asn1gen.parsing.asn1.ast.kind._
-
 case class IdentifierList(
   identifiers: List[Identifier]
-) extends Node with BitStringValueKind {
+) extends Node with BitStringValue {
 }
-
