21cf63716daa2d9c33db46c0a5f277171055d00d
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-07 13:09:46 +0000

    Convert ValueConstraint class into a trait.

--- asn1gen/src/org/asn1gen/parsing/asn1/ast/Constraint.scala
+++ asn1gen/src/org/asn1gen/parsing/asn1/ast/Constraint.scala
@@ -1,9 +1,7 @@
 package org.asn1gen.parsing.asn1.ast
 
-import org.asn1gen.parsing.asn1.ast.kind._
-
 case class Constraint(
   constraintSpec: ConstraintSpec,
   exceptionSpec: ExceptionSpec
-) extends Node with ValueConstraintKind with ConstraintOrSizeConstraint {
+) extends Node with ValueConstraint with ConstraintOrSizeConstraint {
 }
