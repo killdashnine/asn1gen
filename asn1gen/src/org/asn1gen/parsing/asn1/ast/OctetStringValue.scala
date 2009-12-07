cdb99cbd9521e748fba1267fe3eafe44927c1fff
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-07 11:51:44 +0000

    Convert OctetStringValue class into a trait.

--- asn1gen/src/org/asn1gen/parsing/asn1/ast/OctetStringValue.scala
+++ asn1gen/src/org/asn1gen/parsing/asn1/ast/OctetStringValue.scala
@@ -1,8 +1,4 @@
 package org.asn1gen.parsing.asn1.ast
 
-import org.asn1gen.parsing.asn1.ast.kind._
-
-case class OctetStringValue(
-  kind: OctetStringValueKind
-) extends Node with BuiltinValue {
+trait OctetStringValue extends BuiltinValue {
 }
