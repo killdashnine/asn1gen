97a7ef925f765c0792d25c827f5f3da866259733
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-07 11:39:40 +0000

    Convert EnumerationItem class into a trait.

--- asn1gen/src/org/asn1gen/parsing/asn1/ast/Identifier.scala
+++ asn1gen/src/org/asn1gen/parsing/asn1/ast/Identifier.scala
@@ -1,9 +1,7 @@
 package org.asn1gen.parsing.asn1.ast
 
-import org.asn1gen.parsing.asn1.ast.kind._
-
 case class Identifier(
   chars: String
-) extends Node with IntegerValue with EnumerationItemKind {
+) extends Node with IntegerValue with EnumerationItem {
   def name = chars
 }
