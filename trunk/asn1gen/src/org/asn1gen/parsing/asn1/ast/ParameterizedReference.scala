218362b8d6dc7a7a440e19dcf0fb30366788e0f8
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-07 11:07:19 +0000

    Convert Symbol class into a trait.

--- trunk/asn1gen/src/org/asn1gen/parsing/asn1/ast/ParameterizedReference.scala
+++ trunk/asn1gen/src/org/asn1gen/parsing/asn1/ast/ParameterizedReference.scala
@@ -1,10 +1,7 @@
 package org.asn1gen.parsing.asn1.ast
 
-import org.asn1gen.parsing.asn1.ast.kind._
-
 case class ParameterizedReference(
   reference: Reference,
   hasBraces: Boolean
-) extends Node with SymbolKind {
+) extends Node with Symbol {
 }
-
