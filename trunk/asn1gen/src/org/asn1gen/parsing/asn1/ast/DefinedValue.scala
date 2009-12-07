313ec1ef9825881446a6989926597531e3bf2f52
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-07 11:19:55 +0000

    Convert DefinedValue class into a trait.

--- trunk/asn1gen/src/org/asn1gen/parsing/asn1/ast/DefinedValue.scala
+++ trunk/asn1gen/src/org/asn1gen/parsing/asn1/ast/DefinedValue.scala
@@ -2,17 +2,15 @@ package org.asn1gen.parsing.asn1.ast
 
 import org.asn1gen.parsing.asn1.ast.kind._
 
-case class DefinedValue(
-  kind: DefinedValueKind
-) extends Node
-    with AssignedIdentifier
-    with CharsDefnKind
-    with ClassNumberKind
-    with ExceptionIdentification
-    with ObjIdComponentsKind
-    with NamedNumberValue
-    with NumberFormKind
-    with ReferencedValue
-    with RelativeOidComponentsKind
-    with NamedBitKind {
+trait DefinedValue
+  extends AssignedIdentifier
+  with CharsDefnKind
+  with ClassNumberKind
+  with ExceptionIdentification
+  with ObjIdComponentsKind
+  with NamedNumberValue
+  with NumberFormKind
+  with ReferencedValue
+  with RelativeOidComponentsKind
+  with NamedBitKind {
 }
