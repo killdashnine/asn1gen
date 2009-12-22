9e7146251c06958e923cbd30568b424c623d19b8
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-22 13:33:28 +0000

    Introduce testng because junit is unreliable. Fix deprecations warnings in ASN classes.

---
+++ trunk/asn1runtimetest/src/org/asn1gen/runtime/test/TestCodec.scala
@@ -0,0 +1,10 @@
+package org.asn1gen.runtime.test
+
+import org.asn1gen.runtime.codec._
+
+object TestCodec {
+  def main(args : Array[String]) : Unit = {
+    val x = new OctetWindow(Array[Byte](1, 2, 3), 0, 3)
+    println("Hello " + x(0) + " " + x(1) + " " + x(2))
+  }
+}
