9f7e08b76a1e47692de609678d9cc6a2f3cec799
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-23 11:14:05 +0000

    Add DecodingInputStream.spanComponent method. Add Triplet.describes method. Add Triplet.contextSpecific property. Assert.assertThrows, takes extra class element. Add BerDecoder object. Add non-functional BerDecoder.decode method for MySequence type. Add Test_ suite to run both TestBerDecoder and TestOctetWindow.

--- trunk/asn1runtimetest/src/org/asn1gen/junit/Assert.scala
+++ trunk/asn1runtimetest/src/org/asn1gen/junit/Assert.scala
@@ -1,13 +1,16 @@
 package org.asn1gen.junit
 
 trait Assert {
-  def assertThrows[E](f: => Unit): Unit = {
+  def assertThrows[E](eClass: Class[E])(f: => Unit): Unit = {
     try {
       f
     } catch {
-      case e: E => return
+      case e if eClass.isAssignableFrom(e.getClass) => return
       case e => throw e
     }
+    
     throw new Exception("Exception expected")
   }
 }
+
+object Assert extends Assert
---
+++ trunk/asn1runtimetest/src/test/org/asn1gen/runtime/codec/Test_.scala
@@ -0,0 +1,13 @@
+package test.org.asn1gen.runtime.codec
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+ 
+@RunWith(classOf[Suite])
+@Suite.SuiteClasses(Array
+    ( classOf[TestBerDecoder]
+    , classOf[TestOctetWindow]
+    )
+)
+class Test_ {
+}
