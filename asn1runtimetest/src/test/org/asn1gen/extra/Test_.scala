2352641562bb26f6c576774482b8f5cdabd6e396
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-25 05:59:14 +0000

    Add test suite for test.org.asn1gen.extra package.

---
+++ asn1runtimetest/src/test/org/asn1gen/extra/Test_.scala
@@ -0,0 +1,11 @@
+package test.org.asn1gen.extra
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+ 
+@RunWith(classOf[Suite])
+@Suite.SuiteClasses(Array
+    ( classOf[TestByteExtra]
+    )
+)
+class Test_
