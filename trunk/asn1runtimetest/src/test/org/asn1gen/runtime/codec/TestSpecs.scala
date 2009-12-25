76b9ce09ca390661978b6c9a6e27b8fc052c09a3
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2009-12-25 05:16:30 +0000

    Add specs library to allow better unit testing.

---
+++ trunk/asn1runtimetest/src/test/org/asn1gen/runtime/codec/TestSpecs.scala
@@ -0,0 +1,15 @@
+package test.org.asn1gen.runtime.codec
+
+import org.specs._
+import org.specs.runner._
+
+class TestSpecsRunner extends JUnit4(TestSpecs)
+
+object TestSpecs extends Specification with JUnit {
+  "'hello world' has 11 characters" in {
+     "hello world".size must_== 11
+  }
+  "'hello world' matches 'h.* w.*'" in {
+     "hello world" must be matching("h.* w.*")
+  }
+}
