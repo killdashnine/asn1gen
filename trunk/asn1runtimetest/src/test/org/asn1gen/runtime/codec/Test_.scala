28ed9f7a52066a3cb4e111a630f4ec7832772b3b
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2010-01-06 13:05:32 +0000

    Test case demonstrating that parsing of basic sequence type is functional.

--- trunk/asn1runtimetest/src/test/org/asn1gen/runtime/codec/Test_.scala
+++ trunk/asn1runtimetest/src/test/org/asn1gen/runtime/codec/Test_.scala
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses(Array
     ( classOf[TestBerDecoder]
     , classOf[TestOctetWindow]
+    , classOf[TestMyPackratBerDecoder]
     , classOf[TestPackratBerDecoder]
     , classOf[TestSpecs]
     )
