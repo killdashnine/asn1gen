a216514dcdb08825d823434adf8f9c338d9cafa7
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2010-01-08 11:00:36 +0000

    Rename MyPackratBerDecoder to ExemplarDecoder and move to separate file. Rename MyPackratBerRealiser to ExemplarRealiser and move to separate file. Add decoding support for other fields of the MyChoice type. New test cases for MyChoice type via MySequence type.

--- asn1runtimetest/src/test/asn1/genruntime/TestGeneratedCode.scala
+++ asn1runtimetest/src/test/asn1/genruntime/TestGeneratedCode.scala
@@ -38,35 +38,35 @@ package test.asn1.genruntime {
     def choice2: Option[AsnReal] = None
 
     def choice0(f: (MyChoice => AsnNull)): MyChoice =
-      MyChoice.Choice0(f(this))
+      MyChoice_Choice0(f(this))
 
     def choice1(f: (MyChoice => AsnInteger)): MyChoice =
-      MyChoice.Choice1(f(this))
+      MyChoice_Choice1(f(this))
 
     def choice2(f: (MyChoice => AsnReal)): MyChoice =
-      MyChoice.Choice2(f(this))
+      MyChoice_Choice2(f(this))
+  }
+  
+  case class MyChoice_Choice0(_element: AsnNull) extends MyChoice {
+    def _choice: Int = 0
+    
+    override def choice0: Option[AsnNull] = Some(_element)
+  }
+
+  case class MyChoice_Choice1(_element: AsnInteger) extends MyChoice {
+    def _choice: Int = 1
+    
+    override def choice1: Option[AsnInteger] = Some(_element)
+  }
+
+  case class MyChoice_Choice2(_element: AsnReal) extends MyChoice {
+    def _choice: Int = 2
+    
+    override def choice2: Option[AsnReal] = Some(_element)
   }
 
   object MyChoice {
-    object default extends Choice0(AsnNull)
-    
-    case class Choice0(_element: AsnNull) extends MyChoice {
-      def _choice: Int = 0
-      
-      override def choice0: Option[AsnNull] = Some(_element)
-    }
-  
-    case class Choice1(_element: AsnInteger) extends MyChoice {
-      def _choice: Int = 1
-      
-      override def choice1: Option[AsnInteger] = Some(_element)
-    }
-  
-    case class Choice2(_element: AsnReal) extends MyChoice {
-      def _choice: Int = 2
-      
-      override def choice2: Option[AsnReal] = Some(_element)
-    }
+    object default extends MyChoice_Choice0(AsnNull)
   }
 
   import org.asn1gen.runtime._
@@ -119,57 +119,4 @@ package test.asn1.genruntime {
   object OnMySequence extends OnMySequence(OnAsnInteger, OnAsnInteger)
   
   import org.asn1gen.runtime.codec.PackratBerDecoder
-  
-  trait MyPackratBerDecoder extends PackratBerDecoder {
-    type MySequence
-    
-    def mySequence(length: Int): Parser[MySequence] =
-      ( getOffset
-      >>{ offset =>
-          val wallOffset = offset + length
-          val wall = offsetWall(wallOffset)
-          ( ( (rawTagHeader >> where(_.tagType == 0))
-            ~>(rawLength >> asnInteger) <~ wall
-            ).?
-          ~ ( (rawTagHeader >> where(_.tagType == 1))
-            ~>(rawLength >> asnReal) <~ wall
-            )
-          ~ ( (rawTagHeader >> where(_.tagType == 2))
-            ~>(rawLength >> asnPrintableString) <~ wall
-            )
-          ~ ( (rawTagHeader >> where(_.tagType == 3))
-            ~>(rawLength >> myChoice) <~ wall
-            )
-          ) <~ atOffset(wallOffset)
-        }
-      ) ^^ mkMySequence
-    
-    def mkMySequence(data: Option[AsnInteger] ~ AsnReal ~ AsnPrintableString ~ MyChoice): MySequence
-    
-    def myChoice(length: Int): Parser[MyChoice] =
-      ( getOffset
-      >>{ offset =>
-          val wallOffset = offset + length
-          ( ( (rawTagHeader >> where(_.tagType == 0))
-            ~> (rawLength >> asnNull)
-            ) ^^ mkMyChoice_choice0
-          ) <~ atOffset(wallOffset)
-        }
-      )
-
-    def mkMyChoice_choice0(data: AsnNull): MyChoice
-  }
-  
-  trait MyPackratBerRealiser extends PackratBerRealiser with Parsers {
-    type MySequence = (Option[Long], Double, AsnPrintableString, MyChoice)
-    type MyChoice = test.asn1.genruntime.MyChoice
-    
-    def mkMySequence(value: Option[Long] ~ Double ~ AsnPrintableString ~ MyChoice): MySequence = {
-      value match { case a ~ b ~ c ~ d => (a, b, c, d) }
-    }
-
-    def mkMyChoice_choice0(data: AsnNull): MyChoice = {
-      test.asn1.genruntime.MyChoice.Choice0(data)
-    }
-  }
 }
