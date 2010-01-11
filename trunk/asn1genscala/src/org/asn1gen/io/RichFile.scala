a045b2f8761807cb2601f65376548f7faabc4908
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2010-01-11 11:19:30 +0000

    Add NamedType.from method. Produce HashMap for type and value assignments instead of HashSet. Fix file property of RichFile class.

--- trunk/asn1genscala/src/org/asn1gen/io/RichFile.scala
+++ trunk/asn1genscala/src/org/asn1gen/io/RichFile.scala
@@ -3,8 +3,9 @@ package org.asn1gen.io
 import java.io.FileOutputStream
 import java.io.OutputStream
 import java.io.PrintStream
+import org.asn1gen.io.JavaTypes._
 
-case class RichFile(file: RichFile) {
+case class RichFile(file: JavaFile) {
   def openOutputStream[T](f: OutputStream => T): T = {
     val os = new FileOutputStream(file)
     try f(os) finally os.close
