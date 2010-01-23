5f8b671cb4160a19fac10537d3416fd2ee726f83
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2010-01-23 12:10:10 +0000

    Make type of Empty object a case class.

--- trunk/asn1ast/src/org/asn1gen/parsing/asn1/ast/Empty.scala
+++ trunk/asn1ast/src/org/asn1gen/parsing/asn1/ast/Empty.scala
@@ -2,8 +2,7 @@ package org.asn1gen.parsing.asn1.ast
 
 import org.asn1gen.parsing.asn1.ast.kind._
 
-object Empty
-  extends Node
+case class Empty() extends Node
   with AssignedIdentifier
   with Class_
   with ExceptionSpec
@@ -15,4 +14,7 @@ object Empty
   with TagDefault
   with ValueConstraint
   with WithSyntaxSpec {
+}
+
+object Empty extends Empty() {
 }
