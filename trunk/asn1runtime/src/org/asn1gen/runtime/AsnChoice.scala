e2d2920d46e08ba26c2ee98687c109d022b80676
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2010-03-02 09:53:51 +0000

    Add _choice and _element properties so that they are accessible from the AsnChoice class interface.

--- trunk/asn1runtime/src/org/asn1gen/runtime/AsnChoice.scala
+++ trunk/asn1runtime/src/org/asn1gen/runtime/AsnChoice.scala
@@ -2,6 +2,10 @@ package org.asn1gen.runtime
 
 abstract class AsnChoice extends AsnType {
   override def _desc: meta.AsnChoice = meta.AsnChoice
+  
+  def _choiceName: String
+  
+  def _element: Any
 }
 
 object AsnChoice {
