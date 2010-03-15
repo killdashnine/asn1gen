467f1cfa96929cfec43337c62fce771e6ad3dcfe
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2010-03-15 20:58:59 +0000

    Add ability to encode integer.

--- trunk/asn1runtime/src/org/asn1gen/runtime/codec/BerEncoder.scala
+++ trunk/asn1runtime/src/org/asn1gen/runtime/codec/BerEncoder.scala
@@ -1,6 +1,17 @@
 package org.asn1gen.runtime.codec
 
 import java.io._
+import org.asn1gen.io._
 
-class BerEncoder(os: OutputStream) {
+trait BerEncoder {
+  def encodeFixedInteger(value: Long): ByteStreamer = {
+    val b: Byte = value.toByte
+    if (value == 0) {
+      ByteStreamer.nil
+    } else if (value < 256) {
+      ByteStreamer.byte(b)
+    } else {
+      encodeFixedInteger(value >> 8) ::: ByteStreamer.byte(b)
+    }
+  }
 }
