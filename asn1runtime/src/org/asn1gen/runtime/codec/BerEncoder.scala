dee17b15335239ab9c03a2874a8f7453ac29fbd3
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2010-03-15 23:20:20 +0000

    Encoding of fixed integers.

--- asn1runtime/src/org/asn1gen/runtime/codec/BerEncoder.scala
+++ asn1runtime/src/org/asn1gen/runtime/codec/BerEncoder.scala
@@ -4,14 +4,30 @@ import java.io._
 import org.asn1gen.io._
 
 trait BerEncoder {
-  def encodeFixedInteger(value: Long): ByteStreamer = {
-    val b: Byte = value.toByte
+  def encodeFixedMore(value: Long): ByteStreamer = {
     if (value == 0) {
+      ByteStreamer.byte(0)
+    } else if (value == -1) {
       ByteStreamer.nil
-    } else if (value < 256) {
-      ByteStreamer.byte(b)
     } else {
-      encodeFixedInteger(value >> 8) ::: ByteStreamer.byte(b)
+      val b: Byte = value.toByte
+      if (value < 128) {
+        ByteStreamer.byte(b)
+      } else {
+        encodeFixedMore(value >> 8) ::: ByteStreamer.byte(b)
+      }
+    }
+  }
+  
+  def encodeFixed(value: Long): ByteStreamer = {
+    if (value == -1L) {
+      ByteStreamer.byte(-1)
+    } else if (value == 0L) {
+      ByteStreamer.nil
+    } else {
+      encodeFixedMore(value)
     }
   }
 }
+
+object BerEncoder extends BerEncoder
