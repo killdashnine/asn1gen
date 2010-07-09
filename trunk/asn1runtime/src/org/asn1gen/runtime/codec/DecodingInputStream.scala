2507bc736bfb693f6cf6260cf97bc54c999f0c88
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2010-07-09 13:30:51 +0000

    Add val keyword.

--- trunk/asn1runtime/src/org/asn1gen/runtime/codec/DecodingInputStream.scala
+++ trunk/asn1runtime/src/org/asn1gen/runtime/codec/DecodingInputStream.scala
@@ -3,7 +3,7 @@ package org.asn1gen.runtime.codec
 import java.io.InputStream
 import java.io.EOFException
 
-class DecodingInputStream(private _is: InputStream, private var _index: Int = 0) extends InputStream {
+class DecodingInputStream(private val _is: InputStream, private var _index: Int = 0) extends InputStream {
   def index: Int = _index
   
   def read(): Int = {
