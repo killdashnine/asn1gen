6b1f54d19d9f577806ee0597964d1220d505353d
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2010-10-01 14:28:18 +0000

    New bin method.

--- trunk/asn1core/src/org/asn1gen/extra/ExtraString.scala
+++ trunk/asn1core/src/org/asn1gen/extra/ExtraString.scala
@@ -13,4 +13,17 @@ class ExtraString(value: String) {
   }
   
   def capitalise: String = value(0).toUpper + value.substring(1)
+  
+  def bin: Int = {
+    require(0 < value.length, "String length too small for binary")
+    require(value.length < 32, "String length too large for 32-bit binary")
+
+    value.foldLeft(0) { case (n, c) =>
+      c match {
+        case '0' => n << 1
+        case '1' => (n << 1) + 1
+        case _ => throw new Exception("")
+      }
+    }
+  }
 }
