db75c14934ebce56b9b6ee71f7c6835accf9609f
newhoggy <newhoggy@7df697e6-b8ba-11de-9ad3-fd5786690477>
2010-10-09 04:21:49 +0000

    Remove unused asn1genscala.

--- asn1genscala/src/org/asn1gen/gen/scala/DuplicateTypeException.scala
+++
@@ -1,5 +0,0 @@
-package org.asn1gen.gen.scala
-
-class DuplicateTypeException(reason: String, cause: Throwable) extends Exception(reason, cause) {
-  def this(reason: String) = this(reason, null)
-}
--- asn1genscala/src/org/asn1gen/gen/scala/DuplicateValueException.scala
+++
@@ -1,5 +0,0 @@
-package org.asn1gen.gen.scala
-
-class DuplicateValueException(reason: String, cause: Throwable) extends Exception(reason, cause) {
-  def this(reason: String) = this(reason, null)
-}
