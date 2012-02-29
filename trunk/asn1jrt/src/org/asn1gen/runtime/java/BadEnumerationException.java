23d31aec62ed28360e04be77c9b8cdc38169783a
newhoggy@gmail.com <newhoggy@gmail.com@7df697e6-b8ba-11de-9ad3-fd5786690477>
2012-02-29 23:08:19 +0000

    Message and cause constructors for exception class.

--- trunk/asn1jrt/src/org/asn1gen/runtime/java/BadEnumerationException.java
+++ trunk/asn1jrt/src/org/asn1gen/runtime/java/BadEnumerationException.java
@@ -1,5 +1,19 @@
 package org.asn1gen.runtime.java;
 
+@SuppressWarnings("serial")
 public class BadEnumerationException extends Exception {
+  public BadEnumerationException() {
+  }
+  
+  public BadEnumerationException(final String message) {
+    super(message);
+  }
 
+  public BadEnumerationException(final String message, final Throwable cause) {
+    super(message, cause);
+  }
+
+  public BadEnumerationException(final Throwable cause) {
+    super(cause);
+  }
 }
