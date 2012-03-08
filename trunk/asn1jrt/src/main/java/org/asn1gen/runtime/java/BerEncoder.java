567f1bfc108397105b5ff04cc6e964164478b0c9
newhoggy@gmail.com <newhoggy@gmail.com@7df697e6-b8ba-11de-9ad3-fd5786690477>
2012-03-08 14:00:50 +0000

    Code for encoding tag.

--- trunk/asn1jrt/src/main/java/org/asn1gen/runtime/java/BerEncoder.java
+++ trunk/asn1jrt/src/main/java/org/asn1gen/runtime/java/BerEncoder.java
@@ -2,9 +2,6 @@ package org.asn1gen.runtime.java;
 
 
 public class BerEncoder {
-  private static final byte[] FALSE_BYTES = new byte[] { 1, 1, 0 };
-  private static final byte[] NULL_BYTES = new byte[] { 5, 0, 0 };
-  
   public BerEncoder() {
   }
   
@@ -18,6 +15,62 @@ public class BerEncoder {
   
   public BerWriter encode(final AsnNull value) {
     return BerWriter.EMPTY.ibyte(0x05).ibyte(0x00).ibyte(0x00);
+  }
+
+  public BerWriter complexTagId(final BerWriter preceeding, final long tagId) {
+    return complexTagId(preceeding, tagId, 0x00);
+  }
+  
+  public BerWriter complexTagId(final BerWriter preceeding, final long tagId, final int terminus) {
+    final long excessTagId = tagId >>> 7;
+    final int capturedTagId = (int)tagId & 0x7f;
+    if (excessTagId != 0) {
+      return complexTagId(preceeding, excessTagId, 0x80).ibyte(capturedTagId | terminus);
+    } else {
+      return preceeding.lbyte(capturedTagId | terminus);
+    }
+  }
+  
+  public BerWriter tag(final BerWriter preceeding, final AsnClass clazz, final AsnForm form, final long tagId) {
+    if (tagId < 0) {
+      throw new IllegalArgumentException();
+    }
+    
+    int value = 0;
+
+    switch (clazz) {
+    case UNIVERSAL:
+      value |= 0x00; // 0000 0000
+      break;
+    case APPLICATION:
+      value |= 0x40; // 0100 0000
+      break;
+    case CONTEXT_SPECIFIC:
+      value |= 0x80; // 1000 0000
+      break;
+    case PRIVATE:
+      value |= 0xc0; // 1100 0000
+      break;
+    }
+    
+    switch (form) {
+    case PRIMITIVE:
+      value |= 0x00; // 0000 0000
+      break;
+    case CONSTRUCTED:
+      value |= 0x20; // 0010 0000
+      break;
+    }
+    
+    if (tagId <= 0 && tagId <= 30) {
+      value |= (int)tagId;
+      
+      return preceeding.ibyte(value);
+    } else {
+      value |= 0x1f; // 0001 1111
+      
+      return complexTagId(preceeding.ibyte(value), tagId);
+    }
   }
   
   public BerWriter encode(final AsnInteger value) {
