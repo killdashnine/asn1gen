1085158e5689a550b3c0a40c63d014edaf8aaf9c
newhoggy@gmail.com <newhoggy@gmail.com@7df697e6-b8ba-11de-9ad3-fd5786690477>
2012-03-29 13:54:47 +0000

    Printing some octet string, enumeration and boolean types (enumeration integers needs fixing).

--- trunk/asn1jrt/src/main/java/org/asn1gen/runtime/java/TLV.java
+++ trunk/asn1jrt/src/main/java/org/asn1gen/runtime/java/TLV.java
@@ -16,65 +16,143 @@ public class TLV {
     }
   }
 
-  public static void dump(final IndentWriter out, final ByteArrayWindow window) {
+  public static ByteArrayWindow dump(final IndentWriter out, final ByteArrayWindow window) {
     if (window.length == 0) {
-      return;
+      return window;
     }
     
-    final byte tagByte = window.get(0);
+    final Tuple5<TagClass, TagForm, Integer, ByteArrayWindow, ByteArrayWindow> tagTuple = tagOf(window);
+    final ByteArrayWindow tagWindow = tagTuple.d;
+    final ByteArrayWindow afterTagWindow = tagTuple.e;
+    final TagClass tagClass = tagTuple.a;
+    final TagForm tagForm = tagTuple.b;
+    final int tagNo = tagTuple.c;
+    final Tuple3<Integer, ByteArrayWindow, ByteArrayWindow> lengthTuple = lengthOf(afterTagWindow);
+    final int length = lengthTuple.a;
+    final ByteArrayWindow lengthWindow = lengthTuple.b;
+    final ByteArrayWindow afterLengthWindow = lengthTuple.c;
+    final ByteArrayWindow dataWindow = afterLengthWindow.until(length);
+    final ByteArrayWindow remainderWindow = afterLengthWindow.from(length);
     
-    if (window.length == 1) {
-      out.$("<missing tag part>");
-      return;
+    final Delimeter tagDelimeter = new Delimeter("", " ");
+    
+    out.hex(tagWindow).$(' ').$('[').$(tagDelimeter).$(tagClass).$(tagDelimeter).$(tagForm).$(tagDelimeter).$(tagNo).$("]").$(' ');
+    out.hex(lengthWindow).$(' ').$('[').$(length).$(']');
+    
+    if (tagForm == TagForm.PRIMITIVE) {
+      switch (tagNo) {
+      case 1:
+        assert length == 1;
+        out.$(' ').hex(dataWindow).$(' ');
+        out.$("[BOOLEAN:").$(dataWindow.get(0) != 0 ? "true" : "false").$("]");
+        break;
+      case 4: // Octet String
+        assert length == 1;
+        out.$(' ').hex(dataWindow).$(dataWindow.length > 0 ? " " : "");
+        out.$("[OCTET_STRING]");
+        break;
+      case 10:
+        assert length == 1;
+        out.$(' ').hex(dataWindow).$(' ');
+        out.$("[ENUMERATION:").$(intValue(dataWindow)).$("]");
+        break;
+      }
+      out.endln();
+    } else {
+      if (dataWindow.length > 0) {
+        out.$(" {").endln();
+        
+        try (final Indent indent = out.indent(2)) {
+          switch (tagClass) {
+          case UNIVERSAL:
+            switch (tagForm) {
+            case PRIMITIVE:
+            case CONSTRUCTED:
+              if (true) {
+                ByteArrayWindow childWindow = dataWindow;
+                while (childWindow.length > 0) {
+                  childWindow = dump(out, childWindow);
+                }
+                break;
+              }
+            }
+            break;
+          default:
+            out.hex(dataWindow).endln();
+            break;
+          }
+        }
+        
+        out.$("}").endln();
+      } else {
+        out.endln();
+      }
     }
     
-    final ByteArrayWindow afterTagWindow = window.from(1);
-    final int shortTag = tagByte & 0x1f;
-    final Tuple2<Integer, ByteArrayWindow> tagTuple = tagOf(shortTag, afterTagWindow);
-    final ByteArrayWindow lengthWindow = afterTagWindow.until(afterTagWindow.length - tagTuple.b.length);
-    final int tag = tagTuple.a;
-    final TagClass tagClass = TagClass.fromTagByte(tagByte);
-    final TagForm tagForm = TagForm.fromTagByte(tagByte);
-    
-    for (int i = 0; i < lengthWindow.length; ++i) {
-      out.hex(lengthWindow.get(i)).$(' ');
-    }
-    
-    final Delimeter delimeter = new Delimeter("", " ");
-    
-    out.hex(tagByte).$(' ').$('[').$(delimeter).$(tagClass).$(delimeter).$(tagForm).$(delimeter).$(tag).$("] {").endln();
-    
-    try (final Indent indent = out.indent(2)) {
-      out.$("Hello").endln();
-    }
-    
-    out.$("}");
+    return remainderWindow;
   }
   
-  public static Tuple2<Integer, ByteArrayWindow> tagOf(
-      final int shortLength,
-      final ByteArrayWindow window) {
-    if (shortLength < 32) {
-      return new Tuple2<Integer, ByteArrayWindow>(shortLength, window);
+  private static int intValue(final ByteArrayWindow dataWindow) {
+    if (dataWindow.length == 0) {
+      return 0;
+    } else {
+      return dataWindow.get(0);
+    }
+  }
+
+  public static Tuple5<TagClass, TagForm, Integer, ByteArrayWindow, ByteArrayWindow> tagOf(final ByteArrayWindow window) {
+    final byte tagByte = window.get(0);
+    final TagClass tagClass = TagClass.fromTagByte(tagByte);
+    final TagForm tagForm = TagForm.fromTagByte(tagByte);
+    final int shortTag = tagByte & 0x1f;
+    
+    if (shortTag < 32) {
+      return new Tuple5<TagClass, TagForm, Integer, ByteArrayWindow, ByteArrayWindow>(
+          tagClass, tagForm, shortTag, window.until(1), window.from(1));
     }
     
-    return tagOf(window, 0);
+    return tagOf(tagClass, tagForm, 0, window, window.from(1));
   }
     
-  public static Tuple2<Integer, ByteArrayWindow> tagOf(
-      final ByteArrayWindow window,
-      final int accumulator) {
-    if (window.length == 0) {
-      return null;
-    }
-    
+  public static Tuple5<TagClass, TagForm, Integer, ByteArrayWindow, ByteArrayWindow> tagOf(
+      final TagClass tagClass,
+      final TagForm tagForm,
+      final int accumulator,
+      final ByteArrayWindow tagStartWindow,
+      final ByteArrayWindow window) {
     final int value = (accumulator << 7) + (window.get(0) & (int)0x7f);
+    final ByteArrayWindow nextWindow = window.from(1);
     
     if ((window.get(0) & 0x80) != 0) {
-      return tagOf(window.from(1), value);
+      return tagOf(tagClass, tagForm, value, tagStartWindow, nextWindow);
     }
     
-    return new Tuple2<Integer, ByteArrayWindow>(value, window.from(1));
+    return new Tuple5<TagClass, TagForm, Integer, ByteArrayWindow, ByteArrayWindow>(
+        tagClass, tagForm, value, tagStartWindow.until(nextWindow), nextWindow);
+  }
+  
+  public static Tuple3<Integer, ByteArrayWindow, ByteArrayWindow> lengthOf(final ByteArrayWindow window) {
+    return lengthOf(window, window);
+  }
+  
+  public static Tuple3<Integer, ByteArrayWindow, ByteArrayWindow> lengthOf(final ByteArrayWindow startWindow, final ByteArrayWindow window) {
+    final byte lengthByte = window.get(0);
+    
+    if ((lengthByte & 0x80) == 0) {
+      final int shortLength = lengthByte;
+      
+      return new Tuple3<Integer, ByteArrayWindow, ByteArrayWindow>(shortLength, window.until(1), window.from(1));
+    } else {
+      final int lengthLength = lengthByte & 0x7f;
+      final ByteArrayWindow workingWindow = window.from(1);
+      int length = 0;
+      
+      for (int i = 0; i < lengthLength; ++i) {
+        length = (length << 8) | window.get(i);
+      }
+      
+      return new Tuple3<Integer, ByteArrayWindow, ByteArrayWindow>(length, workingWindow.until(lengthLength), workingWindow.from(lengthLength));
+    }
   }
   
   public static void main(final String[] args) {
