2665087ece26164ec4a0e7a9ea2b8ff06f7212cd
newhoggy@gmail.com <newhoggy@gmail.com@7df697e6-b8ba-11de-9ad3-fd5786690477>
2012-04-02 00:26:14 +0000

    Don't track frames.

--- trunk/asn1jrt/src/main/java/org/asn1gen/runtime/java/TlvFrame.java
+++ trunk/asn1jrt/src/main/java/org/asn1gen/runtime/java/TlvFrame.java
@@ -1,21 +1,15 @@
 package org.asn1gen.runtime.java;
 
 public class TlvFrame {
-  public final TlvFrame parent;
   public final TagClass tagClass;
   public final TagForm tagForm;
   public final long tagNo;
   public final long length;
   
-  public TlvFrame(final TlvFrame parent, final TagClass tagClass, final TagForm tagForm, final long tagNo, final long length) {
-    this.parent = parent;
+  public TlvFrame(final TagClass tagClass, final TagForm tagForm, final long tagNo, final long length) {
     this.tagClass = tagClass;
     this.tagForm = tagForm;
     this.tagNo = tagNo;
     this.length = length;
-  }
-
-  public TlvFrame(final TagClass tagClass, final TagForm tagForm, final long tagNo, final long length) {
-    this(null, tagClass, tagForm, tagNo, length);
   }
 }
