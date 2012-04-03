8eb1ec956adbd05bb6d70f26b2af2670e7e0d580
newhoggy <newhoggy@gmail.com>
2012-04-03 00:46:43 +0000

    Introduce remainder field.

--- asn1jrt/src/main/java/org/asn1gen/runtime/java/TlvFrame.java
+++ asn1jrt/src/main/java/org/asn1gen/runtime/java/TlvFrame.java
@@ -8,18 +8,21 @@ public class TlvFrame {
   public final long tagNo;
   public final long length;
   public final ByteArrayWindow data;
+  public final ByteArrayWindow remainder;
   
   public TlvFrame(
       final TagClass tagClass,
       final TagForm tagForm,
       final long tagNo,
       final long length,
-      final ByteArrayWindow data) {
+      final ByteArrayWindow data,
+      final ByteArrayWindow remainder) {
     this.tagClass = tagClass;
     this.tagForm = tagForm;
     this.tagNo = tagNo;
     this.length = length;
     this.data = data;
+    this.remainder = remainder;
   }
   
   public static TlvFrame readFrom(final ByteArrayWindow window) throws IOException {
@@ -34,17 +37,21 @@ public class TlvFrame {
     final TagClass tagClass = TagClass.fromTagByte(firstTagByte);
     final TagForm tagForm = TagForm.fromTagByte(firstTagByte);
     final long tagNo = readTagNo(firstTagByte, is);
-    final long tagLength = readTagLength(is);
+    final int tagLength = readTagLength(is);
     
-    return new TlvFrame(tagClass, tagForm, tagNo, tagLength, is.getWindow());
+    final ByteArrayWindow nextWindow = is.getWindow();
+    
+    return new TlvFrame(
+        tagClass, tagForm, tagNo, tagLength,
+        nextWindow.until(tagLength), nextWindow.from(tagLength));
   }
   
   public TlvFrame firstChild() throws IOException {
-    return readFrom(data.until((int)length));
+    return readFrom(data);
   }
   
   public TlvFrame next() throws IOException {
-    return readFrom(data.from((int)length));
+    return readFrom(remainder);
   }
   
   public static long readTagNo(
@@ -71,14 +78,14 @@ public class TlvFrame {
     }
   }
   
-  public static long readTagLength(final ByteArrayWindowInputStream is) throws IOException {
+  public static int readTagLength(final ByteArrayWindowInputStream is) throws IOException {
     final int firstLengthByte = is.read();
     
     if ((firstLengthByte & 0x80) == 0) {
       return firstLengthByte & 0x7f;
     } else {
       final int lengthLength = firstLengthByte & 0x7f;
-      long accumulatingLength = 0;
+      int accumulatingLength = 0;
       
       for (int i = 0; i < lengthLength; ++i) {
         accumulatingLength = (accumulatingLength << 8) | (is.read() & 0xff);
