82984a1a97a60cd540ddcd0d2dd87f03f9b47062
newhoggy <newhoggy@gmail.com>
2012-04-11 00:53:32 +0000

    Implement decode method.

--- asn1jrt/drop/moo/codec/ASNEXAMPLES/BerToAsn.java
+++ asn1jrt/drop/moo/codec/ASNEXAMPLES/BerToAsn.java
@@ -2,7 +2,11 @@ package moo.codec.ASNEXAMPLES;
 
 import org.asn1gen.runtime.java.AsnException;
 import org.asn1gen.runtime.java.ByteArrayWindow;
+import org.asn1gen.runtime.java.DetailedTlvFrame;
 import org.asn1gen.runtime.java.TLV;
+import org.asn1gen.runtime.java.TagClass;
+import org.asn1gen.runtime.java.TagForm;
+import org.asn1gen.runtime.java.TlvFrame;
 
 import moo.model.ASNEXAMPLES.BookCover;
 
@@ -10,7 +14,22 @@ public class BerToAsn {
   public static BookCover decode(
       final BookCover value,
       final ByteArrayWindow window) throws AsnException {
-    return decodePart(value, window);
+    final DetailedTlvFrame detailedFrame = TLV.readTlv(window);
+    final TlvFrame frame = detailedFrame.frame;
+    
+    if (frame.tagClass != TagClass.UNIVERSAL) {
+      throw new AsnException();
+    }
+    
+    if (frame.tagForm != TagForm.PRIMITIVE) {
+      throw new AsnException();
+    }
+    
+    if (frame.tagNo != 10) {
+      throw new AsnException();
+    }
+    
+    return decodePart(value, frame.value);
   }
   
   public static BookCover decodePart(
